47f160e6b9446de72bef915edea98fb874f2e68f
mugethaaa <robinwaynemacharia@gmail.com>
2024-10-16 12:25:41 +0300

    second commit

---
+++ app/Http/Controllers/Menu2Controller.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Menu2;
+use App\Http\Requests\StoreMenu2Request;
+use App\Http\Requests\UpdateMenu2Request;
+
+class Menu2Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMenu2Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Menu2 $menu2)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Menu2 $menu2)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMenu2Request $request, Menu2 $menu2)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Menu2 $menu2)
+    {
+        //
+    }
+}
